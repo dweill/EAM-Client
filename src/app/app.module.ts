d36f134558e8791866a5e536801c576e06a38968
Daniel Weill <daniel.zalmen@gmail.com>
2018-08-09 06:30:18 -0500

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { Actor } from './actor/actor.component';
+import { Director } from './director/director.component';
+import { Episode } from './episode/episode.component';
+import { Genre } from './genre/genre.component';
+import { Studio } from './studio/studio.component';
+import { Writer } from './writer/writer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Actor,
+    Director,
+    Episode,
+    Genre,
+    Studio,
+    Writer
   ],
   imports: [
     BrowserModule
